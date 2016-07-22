1753aab34b64bf4b9dba6923c3741dcf2b763036
Güray Gürkan <GurayGurkan@users.noreply.github.com>
2016-07-22 11:58:50 +0300

    Add files via upload

---
+++ NRF24L01_RECEIVER.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+#include <nRF24L01p.h>
+
+nRF24L01p receiver(7, 8); //CSN,CE
+
+String message;
+
+void setup() {
+  delay(150);
+  message = "";
+  Serial.begin(9600);
+  Serial.println("RX baglanti saglandi");
+  SPI.begin();
+  delay(10);
+  SPI.setBitOrder(MSBFIRST);
+  delay(10);
+  receiver.channel(90);
+  delay(10);
+  receiver.RXaddress("Artur");
+  delay(10);
+  receiver.init();
+  delay(10);
+  
+}
+
+
+
+void loop() {
+  if (receiver.available()) {
+    receiver.read();
+    receiver.rxPL(message);
+    if (message.length() > 0)
+    {
+      Serial.print(message);
+      message = "";
+    }
+  }
+
+
+}
---
+++ NRF24L01_VERICI.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <nRF24L01p.h>
+
+nRF24L01p transmitter(9, 10); //CSN,CE
+
+String message;
+
+void setup() {
+  delay(150);
+  Serial.begin(9600);
+  Serial.println("TX baglanti saglandi");
+  SPI.begin();
+  SPI.setBitOrder(LSBFIRST);
+  transmitter.channel(90);
+  transmitter.TXaddress("Artur");
+  transmitter.init();
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    char character = Serial.read();
+    //Serial.println(character);
+    if (character == 'g') {
+
+      transmitter.txPL(message);
+      boolean  state = transmitter.send(SLOW);
+      delay(10);
+      Serial.print("Mesaj : '");
+      Serial.print(message);
+      if (state == true)
+        Serial.println(" gonderildi");
+      else
+        Serial.println(" gonderilemedi");
+
+      message = "";
+    }
+    else {
+      message += character;
+    }
+  }
+}
